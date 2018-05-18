3c02ac1fff805aede8250eb49baa66ce143e183d
Julien Duponchelle <julien.duponchelle@trainline.com>
2018-05-18 21:29:16 +0200

    Working cli

--- json_api_doc/__main__.py
+++ json_api_doc/__main__.py
@@ -4,8 +4,22 @@ from . import parse
 
 
 def main():
-    content = sys.stdin.read()
-    content = json.loads(content)
+    if len(sys.argv) > 1:
+        for path in sys.argv[1:]:
+            with open(path) as f:
+                _read_file(f)
+    else:
+        _read_file(sys.stdin)
+
+
+def _read_file(content):
+    try:
+        content = json.load(content)
+    except json.decoder.JSONDecodeError:
+        print("Invalid JSON file", file=sys.stderr)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        sys.exit(1)
     doc = parse(content)
     print(json.dumps(doc, indent=4))
 
