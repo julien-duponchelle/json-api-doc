b0c68e5b8b86fd9b217d151157d935fa7145a604
Antonio Martinović <martinovic.antonio@gmail.com>
2019-03-03 17:39:19 +0100

    Fixes for tests and a couple of new ones

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -55,13 +55,34 @@ def test_encode_object_list():
     }
 
 
+def test_encode_object_without_attributes():
+    data = {
+        "$type": "article",
+        "id": "1"
+    }
+    doc = json_api_doc.encode(data)
+    assert doc == {
+        "data": {
+            "type": "article",
+            "id": "1"
+        }
+    }
+
+
+def test_invalid():
+    with pytest.raises(AttributeError):
+        json_api_doc.parse({"a": 1})
+
+
 def test_encode_object_embedded():
     data = {
         "$type": "article",
         "id": "1",
         "title": "Article 1",
         "author": {
-            "$type": "person"
+            "$type": "people",
+            "id": "9",
+            "name": "Bob"
         }
     }
     doc = json_api_doc.encode(data)
@@ -71,24 +92,66 @@ def test_encode_object_embedded():
             "id": "1",
             "attributes": {
                 "title": "Article 1"
+            },
+            "relationships": {
+                "author": {
+                    "data": { "type": "people", "id": "9" }
+                }
             }
-        }
+        },
+        "included": [{
+            "type": "people",
+            "id": "9",
+            "attributes": {
+                "name": "Bob",
+            }
+        }]
     }
 
 
-def test_encode_object_without_attributes():
-    response = {
-        "@type": "article",
-        "id": "1"
+def test_encode_object_embedded_list():
+    data = {
+        "$type": "article",
+        "id": "1",
+        "title": "Article 1",
+        "comments": [{
+            "$type": "comment",
+            "id": "100",
+            "content": "First"
+        },{
+            "$type": "comment",
+            "id": "101",
+            "content": "Second"
+        }]
     }
-    doc = json_api_doc.parse(response)
+    doc = json_api_doc.encode(data)
     assert doc == {
         "data": {
             "type": "article",
-            "id": "1"
-        }
+            "id": "1",
+            "attributes": {
+                "title": "Article 1"
+            },
+            "relationships": {
+                "comments": {
+                    "data": [
+                        { "type": "comment", "id": "100" },
+                        { "type": "comment", "id": "101" }
+                    ]
+                }
+            }
+        },
+        "included": [{
+            "type": "comment",
+            "id": "100",
+            "attributes": {
+                "content": "First",
+            }
+        },{
+            "type": "comment",
+            "id": "101",
+            "attributes": {
+                "content": "Second",
+            }
+        }]
     }
-
-def test_invalid():
-    with pytest.raises(AttributeError):
-        json_api_doc.parse({"a": 1})
