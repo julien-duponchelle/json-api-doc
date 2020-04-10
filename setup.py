dfeea941ca098a1f367f07bfee7e3663aa32b5c6
Julien Duponchelle <julien@duponchelle.info>
2020-04-10 18:19:27 +0200

    Bump version: 0.12.0 → 0.13.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/noplay/json-api-doc',
-    version='0.12.0',
+    version='0.13.0',
     entry_points={
         'console_scripts': ['jsonapidoc = json_api_doc.__main__:main'],
     }
